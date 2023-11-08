83eeae56367698b9a713be2422d3ba529c2f214e
Fatima Joy Austria <feyaustria.8@gmail.com>
2023-11-08 10:35:22 +0800

    first try

---
+++ Music-player.js
@@ -0,0 +1,13 @@
+let album_artist = document.querySelector('.album-artist');
+let title = document.querySelector('.title');
+let artist = document.querySelector('.artist');
+
+let play = document.querySelector('.play');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+
+let seek_slider = document.querySelector('.seek_slider');
+let current_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+
+
