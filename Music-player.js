5037797a3f35c04f21fd69caadfe2c96655d0575
Fatima Joy Austria <feyaustria.8@gmail.com>
2023-11-15 19:08:36 +0800

    first first

--- Music-player.js
+++ Music-player.js
@@ -1,8 +1,8 @@
-let album_artist = document.querySelector(".album-artist");
+let album_art = document.querySelector(".album_art");
 let title = document.querySelector(".title");
 let artist = document.querySelector(".artist");
 
-let play = document.querySelector(".play");
+let playPause = document.querySelector(".playPause");
 let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
 
@@ -10,15 +10,15 @@ let seek_slider = document.querySelector(".seek_slider");
 let current_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
 
-// Specify globally used values
+
 let track_index = 0;
 let isPlaying = false;
 let updateTimer;
 
-// Create the audio element for the player
-let curr_track = document.createElement('audio');
+
+let current_track = document.createElement('audio');
  
-// Define the list of tracks that have to be played
+
 let track_list = [
   {
     name: "All to well",
@@ -59,130 +59,96 @@ let track_list = [
 ];
 
 function loadTrack(track_index) {
-  // Clear the previous seek timer
+  
   clearInterval(updateTimer);
   resetValues();
- 
-  // Load a new track
-  curr_track.src = track_list[track_index].path;
-  curr_track.load();
- 
-  // Update details of the track
-  album_artist.style.backgroundImage = 
-     "url(" + track_list[track_index].image + ")";
+  current_track.src = track_list[track_index].path;
+  current_track.load();
+  album_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
   title.textContent = track_list[track_index].name;
   artist.textContent = track_list[track_index].artist;
  
- 
-  // Set an interval of 1000 milliseconds
-  // for updating the seek slider
   updateTimer = setInterval(seekUpdate, 1000);
- 
-  // Move to the next track if the current finishes playing
-  // using the 'ended' event
-  curr_track.addEventListener("ended", nextTrack);
-
-  play.addEventListener("click", playTrack);
+  current_track.addEventListener("ended", nextTrack);
+  playPause.addEventListener("click", playPauseTrack);
+  playPause.addEventListener("click", playTrack);
+  playPause.addEventListener("click", pauseTrack);
   prev.addEventListener("click", prevTrack);
   next.addEventListener("click", nextTrack);
-  
-
-  let pause;
-  pause.addEventListener("click", pauseTrack);
-
-
-  function playTrack() {
-    // Switch between playing and pausing
-    // depending on the current state
-    if (!isPlaying) playTrack();
-    else pauseTrack();
-    }
-    
-    function playTrack() {
-    // Play the loaded track
-    curr_track.play();
-    isPlaying = true;
-    
-    // Replace icon with the pause icon
-    play.innerHTML = '<i class="fa-solid fa-circle-pause"></i>';
-    }
-    
-    function pauseTrack() {
-    // Pause the loaded track
-    curr_track.pause();
-    isPlaying = false;
-    
-    // Replace icon with the play icon
-    play.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
-    }
-    
-    function nextTrack() {
-    // Go back to the first track if the
-    // current one is the last in the track list
-    if (track_index < track_list.length - 1)
-      track_index += 1;
-    else track_index = 0;
-    
-    // Load and play the new track
-    loadTrack(track_index);
-    playTrack();
-    }
-    
-    function prevTrack() {
-    // Go back to the last track if the
-    // current one is the first in the track list
-    if (track_index > 0)
-      track_index -= 1;
-    else track_index = track_list.length - 1;
-    
-    // Load and play the new track
-    loadTrack(track_index);
-    playTrack();
-    }}
-
-    function seekTo() {
-      // Calculate the seek position by the
-      // percentage of the seek slider 
-      // and get the relative duration to the track
-      seekTo = curr_track.duration * (seek_slider.value / 100);
-      
-      // Set the current track position to the calculated seek position
-      curr_track.current_time = seekTo;
-      }
-      
-      
-      function seekUpdate() {
-      let seekPosition = 0;
-      
-      // Check if the current track duration is a legible number
-      if (!isNaN(curr_track.duration)) {
-        seekPosition = curr_track.current_time * (100 / curr_track.duration);
-        seek_slider.value = seekPosition;
-      
-        // Calculate the time left and the total duration
-        let currentMinutes = Math.floor(curr_track.current_time / 60);
-        let currentSeconds = Math.floor(curr_track.current_time - currentMinutes * 60);
-        let durationMinutes = Math.floor(curr_track.duration / 60);
-        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
-      
-        // Add a zero to the single digit time values
-        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
-        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
-        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
-      
-        // Display the updated duration
-        current_time.textContent = currentMinutes + ":" + currentSeconds;
-        total_duration.textContent = durationMinutes + ":" + durationSeconds;
-      }
-      }
-// Load the first track in the tracklist
-loadTrack(track_index);
+}
 
 function resetValues() {
   current_time.textContent = "00:00";
   total_duration.textContent = "00:00";
   seek_slider.value = 0;
- }
+ }  
+
+
+  function playPauseTrack() {
+    if (!isPlaying) playTrack();
+    else pauseTrack();
+    }
+    
+    function playTrack() {
+    current_track.play();
+    isPlaying = true;
+    playPause.innerHTML = '<i class="fa-solid fa-circle-pause"></i>';
+    }
+    
+    function pauseTrack() {
+    current_track.pause();
+    isPlaying = false;
+    playPause.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
+    }
+    
+    function nextTrack() {
+    if (track_index < track_list.length - 1)
+      track_index += 1;
+    else track_index = 0;
+    
+    loadTrack(track_index);
+    playTrack();
+    }
+    
+    function prevTrack() {
+    if (track_index > 0)
+      track_index -= 1;
+    else track_index = track_list.length;
+    
+    loadTrack(track_index);
+    playTrack();
+    }
+  
+    function seekTo() {
+      seekTo = current_track.duration * (seek_slider.value / 100);
+      current_track.current_time = seekTo;
+      }
+      
+      function seekUpdate() {
+      let seekPosition = 0;
+
+      if (!isNaN(current_track.duration)) {
+        seekPosition = current_track.current_time * (100 / current_track.duration);
+        seek_slider.value = seekPosition;
+      
+        let currentMinutes = Math.floor(current_track.current_time / 60);
+        let currentSeconds = Math.floor(current_track.current_time - currentMinutes * 60);
+        let durationMinutes = Math.floor(current_track.duration / 60);
+        let durationSeconds = Math.floor(current_track.duration - durationMinutes * 60);
+      
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+  
+        current_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+      }
+      }
+      
+      loadTrack(track_index);
+
+
+
  
       
